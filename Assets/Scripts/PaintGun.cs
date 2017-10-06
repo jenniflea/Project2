9fb6a8ca4d1bfc532c2a0e3cffdfcce9dd14f41d
Jennifer Dailey <jldailey@umich.edu>
2017-10-06 01:47:41 -0400

    Bullet is shooting

---
+++ Assets/Scripts/PaintGun.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PaintGun : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+
+    private GameObject bullet;
+    private Rigidbody bulletRB;
+
+    public void ShootBullet() {
+        if (bullet != null) return;
+        bullet = Instantiate(bulletPrefab, transform.position + 1.5f*transform.forward, transform.rotation*Quaternion.Euler(90,0,0));
+        bulletRB = bullet.GetComponent<Rigidbody>();
+        bulletRB.AddForce(5000*transform.forward);
+    }
+}
