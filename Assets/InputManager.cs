938faf225f6124c5f20699fe96e8a1ac15939d7b
Jennifer Dailey <jldailey@umich.edu>
2017-10-06 01:22:54 -0400

    FPS Controls + XBox Controller properly set up

---
+++ Assets/InputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    public PaintGun paintGun;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            paintGun.ShootBullet();
+        }
+		
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 newInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        transform.position += newInput;
+        //rb.velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 	}
 }
