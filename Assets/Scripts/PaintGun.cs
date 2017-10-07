02ffceba9f116d5be6428fb51c724f473f8a8ac3
Jennifer Dailey <jldailey@umich.edu>
2017-10-07 03:36:57 -0400

    Target works

--- Assets/Scripts/PaintGun.cs
+++ Assets/Scripts/PaintGun.cs
@@ -3,9 +3,24 @@
 public class PaintGun : MonoBehaviour {
 
     public GameObject bulletPrefab;
+    public GameObject targetPrefab;
 
     private GameObject bullet;
     private Rigidbody bulletRB;
+    private GameObject target;
+
+    private void Start() {
+        target = Instantiate(targetPrefab, transform.position, targetPrefab.transform.rotation);
+    }
+
+    private void FixedUpdate() {
+        // Show an estimate of where the bullet will go
+        RaycastHit raycastHit = new RaycastHit();
+        if (Physics.Raycast(transform.position, transform.forward, out raycastHit, 150.0f)) {
+            target.transform.position = raycastHit.point;
+            target.transform.rotation = raycastHit.transform.rotation;
+        }
+    }
 
     public void ShootBullet() {
         if (bullet != null) return;
